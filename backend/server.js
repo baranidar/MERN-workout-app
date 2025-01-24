0440680a6ddc4cf69ef1858813cc7302d94b10d0
aditibaranidar <baranidar@gmail.com>
2025-01-23 18:24:28 -0800

    Finish backend

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,32 @@
 require('dotenv').config()
-const express = require('express')
 
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+
+// express app
 const app = express()
 
-// middleware. Need to give next so it can move on the next middleware
+// middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-  })
-
-//listen to port
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-  })
-
-//simple get for homepage
-app.get('/', (req, res) => {
-res.json({mssg: 'Welcome to the app'})
+  console.log(req.path, req.method)
+  next()
 })
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
